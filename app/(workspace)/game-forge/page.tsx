21d54573f256b94f0286c4bd9ac4bdd8e0bdf9b0
Giorgi kintsurashvili <kintsurashviligaga@gmail.com>
2026-01-30 00:03:59 +0400

    Update page.tsx

--- app/(workspace)/game-forge/page.tsx
+++ app/(workspace)/game-forge/page.tsx
@@ -1,54 +1,8 @@
-1 line selected
-Find in logs
-CtrlF
-23:54:50.132 
-Running build in Portland, USA (West) – pdx1
-23:54:50.133 
-Build machine configuration: 2 cores, 8 GB
-23:54:50.399 
-Cloning github.com/kintsurashviligaga-ops/avatar-g-frontend-v3 (Branch: main, Commit: 8821426)
-23:54:51.154 
-Cloning completed: 754.000ms
-23:54:51.340 
-Restored build cache from previous deployment (2XzztfTVdVDWkiTq31LVfgQ6JJWe)
-23:54:51.692 
-Running "vercel build"
-23:54:52.625 
-Vercel CLI 50.5.2
-23:54:52.891 
-Installing dependencies...
-23:54:54.902 
-23:54:54.903 
-up to date in 2s
-23:54:54.903 
-23:54:54.903 
-25 packages are looking for funding
-23:54:54.904 
-  run `npm fund` for details
-23:54:54.932 
-Detected Next.js version: 14.2.35
-23:54:54.936 
-Running "npm run build"
-23:54:55.032 
-23:54:55.032 
-> avatar-g-workspace@2.0.0 build
-23:54:55.032 
-> next build
-23:54:55.032 
-23:54:55.731 
-  ▲ Next.js 14.2.35
-23:54:55.732 
-23:54:55.749 
-   Creating an optimized production build ...
-23:55:01.190 
-Failed to compile.
-23:55:01.190 
-23:55:01.191 
+00:01:51.226 
+00:01:51.226 
+Import trace for requested module:
+00:01:51.226 
 ./app/(workspace)/game-forge/page.tsx
-23:55:01.191 
-Error: 
-23:55:01.191 
-  x Unexpected token `ServicePageShell`. Expected jsx identifier
-23:55:01.191 
-    ,-[/vercel/path0/app/(workspace)/game-forge/page.tsx:68:1]
-Deployment Summary
+00:01:51.226 
+00:01:51.226 
+./app/page.tsx
