fc678a15caf7b8b72bbcf9a149a984809f63dd91
Giorgi kintsurashvili <kintsurashviligaga@gmail.com>
2026-01-29 19:14:21 +0400

    Trigger rebuild with all dependencies

--- next.config.js
+++ next.config.js
@@ -9,3 +9,4 @@ module.exports = nextConfig
 // Force complete rebuild - 2026-01-27 23:13
 // Clear all caches and regenerate bundle
 // Bypass Vercel build cache
+// Trigger rebuild - 2026-01-29 19:10
